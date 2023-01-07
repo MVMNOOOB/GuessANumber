a0d777c7a353cdef409d37a8d55d12d56780d900
MVMNOOOB <ethanmitchell2005@gmail.com>
2023-01-06 20:28:47 -0500

    -fixed formatting

--- GuessANumber2/go.py
+++ GuessANumber2/go.py
@@ -1,16 +1,17 @@
 
-#imports
+# imports
 import random
 
-#the teens want peggy.
-#global number, decided at runtime
+# the teens want peggy.
+# global number, decided at runtime
 HIDDENNUM = random.randint(1, 100)
 
-#create global int 1 - 100
-#ask user to enter a number
-#if they guess correct then exit
-#if they guess wrong let them know high or low then recurse
+# create global int 1 - 100
+# ask user to enter a number
+# if they guess correct then exit
+# if they guess wrong let them know high or low then recurse
 print("\n\n\n\n\n\n\n\n\n\n\n\n\nGuess a number 1 through 100: ")
+
 
 def askNum():
     """Asks the user for their guess, and checks the type of the input before handing it to the main function. """
@@ -21,6 +22,7 @@ def askNum():
         print("that won't work")
         playGame()
     return uGuesss
+
 
 def playGame():
     """Runs the game itself, present for recursion"""
@@ -35,5 +37,6 @@ def playGame():
     elif (uGuess == HIDDENNUM):
         print("You Got it!")
         exit()
+
 
 playGame()
